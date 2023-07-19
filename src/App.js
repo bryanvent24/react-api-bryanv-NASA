7fc8a00aedbf5ddbc42f52bfee706f9edf4bb5d6
bryanvent24 <bryanventura.0424@gmail.com>
2023-07-19 14:20:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,57 @@ import Container from "@mui/material/Container";
 import CharacterCard from "./CharacterCard";
 import characters from "./protagonists.json";
 import { useState } from "react";
+import { useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import "./App.css";
 
 function App() {
   // state variable to store the click count!
   const [count, setCounter] = useState(0);
+  const [number, setNumber] = useState(0);
+  const [userInput, setUserInput] = useState("");
+  const [definition, setDefinition] = useState("");
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Perform the API fetch request with the user input
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          // Assuming the API response is an array of definitions
+          const firstDefinition = data[0].meanings[0].definitions[0].definition;
+          setDefinition(firstDefinition);
+        } else {
+          setDefinition("No definition found.");
+        }
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setDefinition("Error occurred during API request.");
+      });
+  };
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  function mybuttoninc() {
+    setNumber(number + 1);
+  }
 
   return (
     <div className="App">
@@ -23,39 +70,106 @@ function App() {
         sx={{ borderBottom: "1px solid lightgray" }}
       >
         <Toolbar>
+          <img
+            src="https://1000logos.net/wp-content/uploads/2017/03/NASA_logo_PNG3.png"
+            alt="NASA Logo"
+            style={{
+              color: "red",
+              marginRight: 10,
+              marginBottom: 10,
+              marginTop: 10,
+              width: 85,
+              height: 85,
+            }}
+          />
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Characters Inc
+            Astronomy Picture Of The Day
+            <Typography varient sx={{ flexGrow: 1 }}>
+              Powered by NASA
+            </Typography>
           </Typography>
+
           <Button
             href="#"
             variant="outlined"
             sx={{ my: 1, mx: 1.5 }}
+            style={{ backgroundColor: "#000080", color: "white" }}
             onClick={() => {
+              fetch(
+                "https://uselessfacts.jsph.pl/api/v2/facts/random",
+                requestOptions
+              )
+                .then((response) => response.json())
+                .then(
+                  (result) =>
+                    (document.getElementById("sub-content").innerHTML =
+                      result.text)
+                )
+
+                .catch((error) => console.log("error", error));
+
+              {
+                mybuttoninc();
+              }
               setCounter(count + 1);
             }}
           >
-            COUNT BUTTON
+            Reveal a random fact!
           </Button>
+
+          {/* TextBox to collect usur's info*/}
+
+          <form
+            onSubmit={handleFormSubmit}
+            // style={{ textAlign: "center", alignItems: "center" }}
+          >
+            <TextField
+              style={{ marginTop: 30 }}
+              label="Enter a word"
+              value={userInput}
+              onChange={handleInputChange}
+            />
+
+            <br />
+
+            <button type="submit">Define a Word</button>
+          </form>
         </Toolbar>
       </AppBar>
       <Container maxWidth="md" sx={{ my: 4 }}>
-        <Typography
-          variant="h2"
-          align="center"
-          color="text.primary"
-          sx={{ py: 2 }}
-        >
-          Prevalent Protagonists
+        <Typography variant="h6" align="center" color="White" sx={{ py: 2 }}>
+          <a href="https://apod.nasa.gov/apod/archivepix.html">
+            Discover the cosmos!
+          </a>{" "}
+          Each day a different image or photograph of our fascinating universe
+          is featured, along with a brief explanation written by a professional
+          astronomer.
         </Typography>
         <Typography
           variant="h5"
           align="center"
-          color="text.secondary"
+          color="white"
           sx={{ mx: 10 }}
-        >
-          Counter: {count}
-        </Typography>
+          id="sub-content"
+        ></Typography>
       </Container>
+      {/* Display the definition */}
+      <div style={{ textAlign: "center" }}>
+        <TextField
+          label="Definition"
+          style={{
+            textAlign: "center",
+
+            marginLeft: 50,
+          }}
+          variant="standard"
+          multiline
+          value={definition}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
       {/* End hero unit */}
       <Container maxWidth="lg">
         <Grid
@@ -64,15 +178,14 @@ function App() {
           justifyContent="center"
           alignItems="flex-start"
         >
-          {characters.map((character) => (
-            <Grid item xs={12} md={4} key={character.title}>
-              <CharacterCard
-                characterName={character.title}
-                image={character.pic}
-                descriptionArray={character.description}
-              />
-            </Grid>
-          ))}
+          <Grid item xs={12} md={4} key={characters[0].title}>
+            <CharacterCard
+              characterName={characters[0].title}
+              image={characters[0].pic}
+              descriptionArray={characters[0].description}
+              date={characters[0].date}
+            />
+          </Grid>
         </Grid>
       </Container>
     </div>
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -15,46 +15,67 @@ var requestOptions = {
 
 export default function CharacterCard(props) {
   // Each card is initialized as a Disney character until card button is pressed.
-  const [catImg, setCatImg] = useState(props.image);
-  const [catBreed, setCatBreed] = useState(props.characterName);
+  const [eventImg, setEventImg] = useState(props.image);
+  const [eventTitle, setEventTitle] = useState(props.characterName);
   const [description, setDescription] = useState(props.descriptionArray);
+  const [date, setDate] = useState(props.date);
 
-  async function getCat() {
+  let newDate = date + "->";
+
+  async function getEvent() {
     // Get a random cat image
     // the "has_breeds=1" is important for the second part
     const response = await fetch(
-      "https://api.thecatapi.com/v1/images/search?has_breeds=1",
+      "https://api.nasa.gov/planetary/apod?api_key=SgHMgglehkV2hudhWe6TZWBYcq5ASGtlJScsXNaK",
       requestOptions
     );
-    const data = await response.json();
+    const eventdata = await response.json();
     // get the first cat in the response data array
-    const catData = data[0];
+    const eventData = eventdata[0];
 
     // Save URL of cat image
-    setCatImg(catData.url);
+    setEventImg(eventdata.url);
 
     // Use the cat's ID to make a new request for cat info
-    const catId = catData.id;
-    const infoResponse = await fetch(
-      `https://api.thecatapi.com/v1/images/${catId}`,
-      requestOptions
-    );
-    const catInfo = await infoResponse.json();
-    setCatBreed(catInfo.breeds[0].name);
+    // const catId = catData.id;
+    // const infoResponse = await fetch(
+    //   `https://api.thecatapi.com/v1/images/${catId}`,
+    //   requestOptions
+    // );
+    //const catInfo = await infoResponse.json();
+    setEventTitle(eventdata.title);
 
     // split the string into an array so we make bullet points later
-    const temperamentList = catInfo.breeds[0].temperament.split(", ");
-    setDescription(temperamentList);
+    //const temperamentList = catInfo.breeds[0].temperament.split(", ");
+    setDescription([eventdata.explanation]);
+
+    setDate([eventdata.date]);
+
+    // const [cutePet, setEvent] = useState();
+    // function getEvent() {
+    //   var requestOptions = {
+    //     method: "GET",
+    //     redirect: "follow",
+    //   };
+
+    //   fetch(
+    //     "https://api.nasa.gov/planetary/apod?api_key=SgHMgglehkV2hudhWe6TZWBYcq5ASGtlJScsXNaK",
+    //     requestOptions
+    //   )
+    //     .then((response) => response.json())
+    //     .then((result) => setEvent(result[0].url))
+    //     .catch((error) => console.log("error", error));
   }
 
   return (
-    <Card>
-      <CardMedia component="img" height="350px" image={catImg} />
+    <Card sx={{ width: "500px", align: "center" }}>
+      <CardMedia component="img" height="500px" image={eventImg} />
       <CardHeader
-        title={catBreed}
+        title={newDate + "" + " " + " " + eventTitle}
         titleTypographyProps={{ align: "center" }}
         sx={{ mt: 1 }}
       />
+
       <CardContent sx={{ pt: 0 }}>
         <ul>
           {description.map((sentence) => (
@@ -69,10 +90,10 @@ export default function CharacterCard(props) {
           variant="contained"
           sx={{ px: 6, mx: "auto" }}
           onClick={() => {
-            getCat();
+            getEvent();
           }}
         >
-          GET CAT
+          GET EVENT
         </Button>
       </CardActions>
     </Card>
